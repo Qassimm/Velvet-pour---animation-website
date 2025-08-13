0eb98d416f99e3cbbf946a97d587751aaa161817
Muhammad Qasim <muhammadqqaasim@gmail.com>
2025-08-13 16:47:17 +0500

    Animated website

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react'
 import Lenis from 'lenis';
 import {ScrollTrigger, SplitText} from 'gsap/all';
 import gsap from 'gsap';
-import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
 import Menu from './components/Menu';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -37,7 +37,7 @@ const App = () => {
 
   return (
     <main>
-      <Navbar />
+     <Navbar
       <Hero />
       <Cocktails />
       <About  />
